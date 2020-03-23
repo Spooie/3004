a066c201ddf15dd05c288a7c9cd9ee0999762c0c
WalshVaz <62193091+WalshVaz@users.noreply.github.com>
2020-03-22 20:47:45 -0400

    Add files via upload

--- electrodes/mainwindow.cpp
+++ electrodes/mainwindow.cpp
@@ -23,10 +23,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::skinOff()
 {
-    time->start(1000);
+    time->start(1000); // timer starts
 
-    timer = timer.addSecs(1);
-    QString stringTime = timer.toString("hh : mm : ss");
+    timer = timer.addSecs(1); // adds 1 second
+    QString stringTime = timer.toString("hh : mm : ss"); // converts the time object to a string
     ui->label->setText(stringTime);
     ui->label_2->setText("SKIN ON");
 
@@ -37,12 +37,13 @@ void MainWindow::skinOff()
 void MainWindow::on_pushButton_clicked()
 {  
 
-    if (time->isActive()){
-        time->stop();
+    if (time->isActive()){ // when timer is running
+        time->stop(); // stops the timer
+        timer.setHMS(0,0,0); // resets timer
         ui->label_2->setText("SKIN OFF");
     }
     else{
-        time->start();
+        time->start(); // start time
     }
 }
 
